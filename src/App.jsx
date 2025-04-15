9c7a7d1a487dfc182760438fc0e12453e05b3418
TaruNipuli <taru.nipuli@metropolia.fi>
2025-04-15 09:46:21 +0300

    Add login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import {Route, BrowserRouter, Routes} from 'react-router';
-import Layout from './components/Layout';
+import './App.css';
+
+import {BrowserRouter, Route, Routes} from 'react-router';
+
 import Home from './views/Home';
-import Upload from './views/Upload';
+import Layout from './components/Layout';
+import Login from './views/Login';
+import Logout from './views/Logout';
 import Profile from './views/Profile';
 import Single from './views/Single';
+import Upload from './views/Upload';
 
-
-const App = () => {
+function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/single" element={<Single />} />
-        <Route path="/" element={<Home />} />
-
-      </Route>
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
+
 export default App;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,29 +1,32 @@
-import { Outlet, Link } from 'react-router';
+import {Link, Outlet} from 'react-router';
 
 const Layout = () => {
-    return (
+  return (
     <div>
-        <header>
+      <header>
         <h1>My App</h1>
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/profile">Profile</Link>
-      </li>
-      <li>
-        <Link to="/upload">Upload</Link>
-      </li>
-    </ul>
-  </nav>
-  </header>
-  <main>
-    <Outlet />
-  </main>
-</div>
-    );
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/upload">Upload</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
